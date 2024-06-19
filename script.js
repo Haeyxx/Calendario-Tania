4fd5c292219ed80358607542dfba2e172845a215
Haeyxx <tania.conde@escoladisseny.net>
2024-06-19 16:10:10 +0200

    Updfuuuck

--- script.js
+++ script.js
@@ -65,10 +65,12 @@ const handleDayInteraction = (event) => {
 // Function to attach event listeners for day clicks or touches
 const attachDayClickListeners = () => {
     const days = document.querySelectorAll('.days li');
-    const interactionEvent = isMobileDevice() ? 'touchstart' : 'click';
-    
     days.forEach(day => {
-        day.addEventListener(interactionEvent, handleDayInteraction);
+        if (isMobileDevice()) {
+            day.addEventListener('touchstart', handleDayInteraction);
+        } else {
+            day.addEventListener('click', handleDayInteraction);
+        }
     });
 };
 
